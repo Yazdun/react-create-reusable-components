7b852e4838a3b8d000576a338d942cfa00f29fbd
Yazdun <yazduntube@gmail.com>
2023-02-28 14:37:20 +0330

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,9 @@ import { useState } from 'react'
 import { Button, Container, Input } from './components'
 
 function App() {
-  const [name, setName] = useState('')
-
-  const handleNameChange = event => setName(event.target.value)
-
   return (
     <div>
-      <Container as="div" className="custom-container bg-secondary">
-        <h2>{name || 'No name provided !'}</h2>
-        <p className="text-secondary">
-          This is subtitle for container 1 ! how cool is that?
-        </p>
-        <Input
-          label="Name"
-          id="name"
-          type="text"
-          value={name}
-          onChange={handleNameChange}
-        />
-        <Button primary onClick={() => setName('Button is clicked')}>
-          Click me baby !
-        </Button>
-      </Container>
+      <h1>Creating Reusable React Components</h1>
     </div>
   )
 }
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,3 @@
-export const Container = ({
-  as: Element = 'div',
-  children,
-  className,
-  ...rest
-}) => {
-  return (
-    <Element {...rest} className={`test ${className}`}>
-      {children}
-    </Element>
-  )
+export const Container = () => {
+  return <div>Container</div>
 }
